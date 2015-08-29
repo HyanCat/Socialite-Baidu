11ee8d742c408d0425aa40ae9963cceb169ee9ec
HyanCat <hyancat@live.cn>
2015-08-29 21:42:24 +0800

    make it works.

--- src/Provider.php
+++ src/Provider.php
@@ -9,70 +9,70 @@ use Laravel\Socialite\Two\User;
 class Provider extends AbstractProvider implements ProviderInterface
 {
 	/**
-     * {@inheritdoc}.
-     */
-    protected function getAuthUrl($state)
-    {
-        return $this->buildAuthUrlFromBase('http://openapi.baidu.com/oauth/2.0/authorize', $state);
-    }
+	 * {@inheritdoc}.
+	 */
+	protected function getAuthUrl($state)
+	{
+		return $this->buildAuthUrlFromBase('http://openapi.baidu.com/oauth/2.0/authorize', $state);
+	}
 
-    /**
-     * {@inheritdoc}.
-     */
-    protected function getTokenUrl()
-    {
-        return 'https://openapi.baidu.com/oauth/2.0/token';
-    }
+	/**
+	 * {@inheritdoc}.
+	 */
+	protected function getTokenUrl()
+	{
+		return 'https://openapi.baidu.com/oauth/2.0/token';
+	}
 
-    /**
-     * {@inheritdoc}.
-     */
-    protected function getUserByToken($token)
-    {
-        $response = $this->getHttpClient()->get('https://openapi.baidu.com/rest/2.0/passport/users/getInfo', [
-            'query' => [
-                'access_token' => $token,
-            ],
-        ]);
+	/**
+	 * {@inheritdoc}.
+	 */
+	protected function getUserByToken($token)
+	{
+		$response = $this->getHttpClient()->get('https://openapi.baidu.com/rest/2.0/passport/users/getInfo', [
+			'query' => [
+				'access_token' => $token,
+			],
+		]);
 
-        return json_decode($response->getBody()->getContents(), true);
-    }
+		return json_decode($response->getBody()->getContents(), true);
+	}
 
-    /**
-     * {@inheritdoc}.
-     */
-    protected function mapUserToObject(array $user)
-    {
-        return (new User())->setRaw($user)->map([
-            'id' => $user['userid'],
-            'nickname' => $user['username'],
-            'avatar' => $user['portrait'],
-            'name' => $user['realname'],
-            'email' => null,
-        ]);
-    }
+	/**
+	 * {@inheritdoc}.
+	 */
+	protected function mapUserToObject(array $user)
+	{
+		return (new User())->setRaw($user)->map([
+			'id'       => $user['userid'],
+			'nickname' => $user['username'],
+			'avatar'   => $this->getPortraitUrl($user['portrait']),
+			'name'     => null,
+			'email'    => null,
+		]);
+	}
 
-    /**
-     * {@inheritdoc}.
-     */
-    protected function getTokenFields($code)
-    {
-        return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code',
-        ]);
-    }
+	/**
+	 * {@inheritdoc}.
+	 */
+	protected function getTokenFields($code)
+	{
+		return array_merge(parent::getTokenFields($code), [
+			'grant_type' => 'authorization_code',
+		]);
+	}
 
-    /**
-     * {@inheritdoc}.
-     */
-    public function getAccessToken($code)
-    {
-        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
-            'query' => $this->getTokenFields($code),
-        ]);
+	/**
+	 * {@inheritdoc}.
+	 */
+	public function getAccessToken($code)
+	{
+		$response = $this->getHttpClient()->post($this->getTokenUrl(), [
+			'query' => $this->getTokenFields($code),
+		]);
 
-        return $this->parseAccessToken($response->getBody()->getContents());
-    }
+		return $this->parseAccessToken($response->getBody()->getContents());
+	}
 
 
 	protected function getPortraitUrl($portrait)
